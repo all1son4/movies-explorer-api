199a50e55072f34c4f081e9fe89a465aeb57746d
all1son4 <dm.strok98@gmail.com>
2022-07-28 12:11:59 +0300

    fix: cookie

--- controllers/users.js
+++ controllers/users.js
@@ -9,108 +9,109 @@ const ConflictError = require('../errors/conflict-error');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const getUser = (req, res, next) => {
-    const id = req.user._id;
+  const id = req.user._id;
 
-    return User
-        .findById(id)
-        .orFail(new NotFoundError(`Пользователь с id: ${id} не найден`))
-        .then((user) => res.status(200)
-            .send(user))
-        .catch((err) => {
-            next(err);
-        });
+  return User
+    .findById(id)
+    .orFail(new NotFoundError(`Пользователь с id: ${id} не найден`))
+    .then((user) => res.status(200)
+      .send(user))
+    .catch((err) => {
+      next(err);
+    });
 };
 
 const updateUser = (req, res, next) => {
-    const id = req.user._id;
-    const { name, email } = req.body;
+  const id = req.user._id;
+  const { name, email } = req.body;
 
-    User
-        .findByIdAndUpdate(
-            id,
-            {
-                name,
-                email
-            },
-            {
-                new: true,
-                runValidators: true
-            },
-        )
-        .orFail(new NotFoundError(`Пользователь с id ${id} не найден`))
-        .then((user) => res.send(user))
-        .catch((err) => {
-            if (err.name === 'ValidationError') {
-                next(new BadRequestError(`${Object.values(err.errors)
-                    .map((error) => error.message)
-                    .join(', ')}`));
-            } else {
-                next(err);
-            }
-        });
+  User
+    .findByIdAndUpdate(
+      id,
+      {
+        name,
+        email
+      },
+      {
+        new: true,
+        runValidators: true
+      },
+    )
+    .orFail(new NotFoundError(`Пользователь с id ${id} не найден`))
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError(`${Object.values(err.errors)
+          .map((error) => error.message)
+          .join(', ')}`));
+      } else {
+        next(err);
+      }
+    });
 };
 
 const createUser = (req, res, next) => {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    bcrypt
-        .hash(password, 10)
-        .then((hash) => User.create({
-            name,
-            email,
-            password: hash,
-        }))
-        .then((data) => res.status(201)
-            .send(data))
-        .catch((err) => {
-            if (err.name === 'ValidationError') {
-                next(new BadRequestError(`${Object.values(err.errors)
-                    .map((error) => error.message)
-                    .join(', ')}`));
-            } else if (err.code === 11000) {
-                next(new ConflictError('Пользователь с данным email уже существует'));
-            } else {
-                next(err);
-            }
-        });
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      email,
+      password: hash,
+    }))
+    .then((data) => res.status(201)
+      .send(data))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError(`${Object.values(err.errors)
+          .map((error) => error.message)
+          .join(', ')}`));
+      } else if (err.code === 11000) {
+        next(new ConflictError('Пользователь с данным email уже существует'));
+      } else {
+        next(err);
+      }
+    });
 };
 
 const signin = (req, res, next) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    return User
-        .findUserByCredentials(email, password)
-        .then((user) => {
-            const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'jwtsecret');
+  return User
+    .findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'jwtsecret');
 
-            res.cookie('jwt', token, {
-                expire: 3600000 * 24 * 7 + Date.now(),
-                httpOnly: true,
-                sameSite: 'None',
-                secure: true,
-            })
-                .send(user.toJSON());
-        })
-        .catch(next);
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7 + Date.now(),
+        httpOnly: true,
+        sameSite: 'None',
+        secure: true,
+      })
+        .send(user.toJSON());
+    })
+    .catch(next);
 };
 
 const signout = (req, res) => {
-    // res.cookie('jwt', 'none', {
-    //     expire: new Date('1970-01-01T00:00:00Z'),
-    //     httpOnly: true,
-    //     sameSite: 'None',
-    //     secure: true,
-    // });
-    // res.clearCookie('jwt');
-  res.cookie('jwt', '', {maxAge: 1})
-    res.status(200)
-        .send({ message: 'Токен удален' });
+  res.cookie('jwt', 'none', {
+    // expire: new Date(Date.now() + 5 * 1000),
+    maxAge: (1),
+    httpOnly: true,
+    sameSite: 'None',
+    secure: true,
+  });
+  // res.clearCookie('jwt');
+  // res.cookie('jwt', '', {maxAge: 1})
+  res.status(200)
+    .send({ message: 'Токен удален' });
 };
 
 module.exports = {
-    getUser,
-    updateUser,
-    createUser,
-    signin,
-    signout,
+  getUser,
+  updateUser,
+  createUser,
+  signin,
+  signout,
 };
