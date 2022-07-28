2f4568614a7ad16c1b33a8d794ede76c075735e4
all1son4 <dm.strok98@gmail.com>
2022-07-28 15:15:51 +0300

    fix: cookie

--- controllers/users.js
+++ controllers/users.js
@@ -102,7 +102,8 @@ const signout = (req, res) => {
   // res.clearCookie('jwt',  {
   //   maxAge: 1, httpOnly: true, sameSite: 'None', secure: true,
   // })
-  res.cookie('jwt', '',{
+  res.clearCookie()
+  res.cookie('jwt', ' ',{
     maxAge: 1,
     httpOnly: true,
     sameSite: 'None',
